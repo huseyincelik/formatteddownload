eb900ff96ac9d6fbc26fa81694455396e5d28fd1
Huseyin Celik <huseyincelik.se@gmail.com>
2015-12-04 10:30:53 +0200

    example use and some fixes

--- download_logs.py
+++ download_logs.py
@@ -1,9 +1,22 @@
 import urllib
 import sys
-import itertools
+import itertools as it
 import time
 import os
 
+# first parameter is the base link
+# second parameter is the limit for generated numbers. default is 10
+# example use: python http://arsiv.aa.com/abc/def{0:02d}/def{1:02d}fff.zip 3
+# downloaded urls for example:
+# http://arsiv.aa.com/abc/def00/def00fff.zip
+# http://arsiv.aa.com/abc/def00/def01fff.zip
+# http://arsiv.aa.com/abc/def00/def02fff.zip
+# http://arsiv.aa.com/abc/def01/def00fff.zip
+# http://arsiv.aa.com/abc/def01/def01fff.zip
+# http://arsiv.aa.com/abc/def01/def02fff.zip
+# http://arsiv.aa.com/abc/def02/def00fff.zip
+# http://arsiv.aa.com/abc/def02/def01fff.zip
+# http://arsiv.aa.com/abc/def02/def02fff.zip
 start_time = time.time()
 
 file_count = 1
@@ -12,14 +25,23 @@ times = 10
 if len(sys.argv) > 2:
     times = int(sys.argv[2])
 
-generated_range = range(1, times + 1)
+generated_range = range(0, times)
 argument_count = url_with_format.count("d}")
-arg_space = itertools.permutations(generated_range, argument_count)
-sample = itertools.permutations(range(0, argument_count), argument_count)
-first_url = url_with_format.format(list(sample)[0])
+
+arg_space = []
+
+for i in range(argument_count):
+    arg_space.append(generated_range)
+
+arguments = it.product(*arg_space)
+sample = it.product(*arg_space)
+
+
+params = list(sample)[0]
+first_url = url_with_format.format(*params)
 filename, extension = os.path.splitext(first_url)
-for args in arg_space:
-    url = url_with_format.format(args)
+for args in arguments:
+    url = url_with_format.format(*args)
     response = urllib.urlopen(url)
     if response.getcode() == 404:
         urllib.urlretrieve(url, "file" + str(file_count) + extension)
